3729a1bd73120e81fe594feb7f33ed1d04165d87
Naioku <adrian.komuda.dev@gmail.com>
2024-07-27 11:06:40 +0200

    Added GameManager.

---
+++ DPTeamGameTask/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace DPTeam
+{
+    [System.Serializable]
+    public class GameManager
+    {
+        public void StartGame()
+        {
+            Debug.Log("StartGame");
+        }
+
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
--- DPTeamGameTask/Assets/Scripts/Managers.cs
+++ DPTeamGameTask/Assets/Scripts/Managers.cs
@@ -7,6 +7,7 @@ namespace DPTeam
     {
         public static Managers Instance { get; private set; }
 
+        [field: SerializeField] public GameManager GameManager { get; private set; }
         public UpdateManager UpdateManager { get; private set; }
 
         private void Awake()
@@ -27,5 +28,12 @@ namespace DPTeam
         private void Update() => UpdateManager.UpdateActions.InvokeActions();
         private void FixedUpdate() => UpdateManager.FixedUpdateActions.InvokeActions();
         private void LateUpdate() => UpdateManager.LateUpdateActions.InvokeActions();
+        
+        // Todo: Tests. Delete after UI implementation.
+        [ContextMenu("StartGame")]
+        private void StartGame() => GameManager.StartGame();
+
+        [ContextMenu("QuitGame")]
+        private void QuitGame() => GameManager.QuitGame();
     }
 }
